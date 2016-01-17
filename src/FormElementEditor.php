8b57d2bce6dded66ad108e37d32067b7866e71ca
Andreas Bratfisch <duskman72@gmail.com>
2016-01-17 13:42:24 +0100

    ad editor

---
+++ src/FormElementEditor.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Simplified\Forms;
+
+class FormElementEditor extends FormElementTextArea {
+	private $height = 300;
+	private $resize = false;
+	private $statusbar = false;
+	
+	public function setHeight($height) {
+		$this->height = intval($height) > 0 ? intval($height) : 200;
+	}
+	
+	public function getHeight() {
+		return $this->height;
+	}
+	
+	public function setResize($resize) {
+		$this->resize = $resize ? true : false;
+	}
+	
+	public function getResize() {
+		return $this->resize ? true : false;
+	}
+	
+	public function render() {
+		$content = parent::render();
+		
+		// add JS to start editor
+		$content .= '
+		<script type="text/javascript">
+			tinymce.init({
+			selector: "textarea#'.$this->getAttribute('id').'",
+			height: '.$this->height.',
+			resize: '.($this->resize ? "true" : "false").',
+			statusbar: '.($this->statusbar ? "true" : "false").',
+			setup: function (editor) {
+				editor.on("change", function () {
+					editor.save();
+				});
+			}
+		});
+		</script>
+		';
+		
+		return $content;
+	}
+}
+
+?>
